53975a031bc4952fb4d60f506f14dd3eb6f6cdee
Jamie Chapman <z5311614@ad.unsw.edu.au>
2025-10-18 19:42:52 +1100

    fixed issues with auto-pairs and vim-surround

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -1,8 +1,25 @@
 return {
     "rafamadriz/friendly-snippets",
-    "tpope/vim-surround",
+    {
+    -- vim surround mnemonics:
+    -- ysiw 'you surround inner word' (insert surrounds e.g ysiw" on hello -> "hello")
+    -- cs 'change surround' cs"' "hello" -> 'hello'
+    -- ds 'delete surround' ds' 'hello' -> hello
+    -- yss 'you surround surround' wraps the whole line ( hello ) ->  {( hello )}
+    --
+    -- For tags, use tag name e.g. <q>. Can use generic 't' for tag when changing/deleting
+    --
+    -- note: '{' adds a space, '}' does not. (works for all bracer types)
+    -- e.g ysiw( on hello -> ( hello ), v.s. ysiw) hello -> (hello)
+    "tpope/vim-surround"
+    },
+
     "vim-scripts/ReplaceWithRegister",
     "numToStr/Comment.nvim",
     "onsails/lspkind.nvim",
-    "windwp/nvim-autopairs",
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = true,
+    },
 }
